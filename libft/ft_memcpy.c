e10fdedd9510fb8246dcb24c89808de01ca51ce0
Manuel Farto Gayoso <mafarto-@c3r4s5.42urduliz.com>
2022-09-26 20:41:21 +0200

    memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,16 +6,21 @@
 /*   By: mafarto- <mafarto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 18:20:50 by mafarto-          #+#    #+#             */
-/*   Updated: 2022/09/26 18:29:33 by mafarto-         ###   ########.fr       */
+/*   Updated: 2022/09/26 20:33:29 by mafarto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memcpy(void *dsr, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    while ()
-    {
-        
-    }
+	size_t	i;
+
+	i = 0;
+	while (i < n && (dst || src))
+	{
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dst);
 }
