cc177bf39265e6a4bfb30433f5f58dffc82fafb9
Manuel Farto Gayoso <mafarto-@c3r3s4.42urduliz.com>
2023-05-24 16:40:45 +0200

    Mediosolucionado

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mafarto- <mafarto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 17:39:55 by mafarto-          #+#    #+#             */
-/*   Updated: 2023/05/22 18:22:11 by mafarto-         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:27:39 by mafarto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1
+# define BUFFER_SIZE 25
 #endif
 
 size_t	ft_strlen(const char *s)
@@ -53,18 +53,43 @@ char	*ft_strjoin(char *s1, char *s2)
 	dst[i] = '\0';
 	return (dst);
 }
-/* char	*ft_read()
+
+char	*ft_read(int fd, char *str, char *line)
 {
-	
-} */
+	ssize_t	rea;
+	char	*tmp;
+
+	rea = read(fd, str, BUFFER_SIZE);
+	if (rea <= 0)
+		return (0);
+	str[rea] = '\0';
+	tmp = line;
+	free(line);
+	line = NULL;
+	line = ft_strjoin(tmp, str);
+	return (line);
+}
+
+int	ft_comper(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 char	*get_next_line(int fd)
 {
-	char		str[256];
-	char static	*line;
+	char		str[BUFFER_SIZE + 1];
+	static char	*line;
 	int			nu;
 	ssize_t		rea;
-	char		*tmp;
 
 	if (!BUFFER_SIZE)
 		return (NULL);
@@ -73,17 +98,12 @@ char	*get_next_line(int fd)
 	if (rea <= 0)
 		return (0);
 	str[BUFFER_SIZE] = '\0';
-	line = ft_strjoin("", str);
+	if (!line)
+		line = ft_strjoin("", str);
 	nu = -1;
-	while (line[++nu] != '\n')
+	while (ft_comper(line) == 0)
 	{
-		rea = read(fd, str, BUFFER_SIZE);
-		if (rea <= 0)
-			break ;
-		str[rea] = '\0';
-		tmp = line;
-		line = ft_strjoin(tmp, str);
-		free(tmp);
+		line = ft_read(fd, str, line);
 	}
 	return (line);
 }
@@ -91,24 +111,25 @@ char	*get_next_line(int fd)
 int	main(void)
 {
 	int	fd;
-	int	fd2;
+	//int	fd2;
 	int	i = 0;
 	char *str;
-	char *str2;
-	fd = open("textpru", O_RDWR);
-	fd2 = open("texto2", O_RDWR);
-	printf("\n%s\n\n", "hola");
-	while (i != 4)
-	{
-		printf("Numero reps: %i\n", i + 1);
-		str = get_next_line(fd);
-		printf("&\n%s&\n", str);
-		str2 = get_next_line(fd2);
-		printf("&\n%s&\n", str2);
-		i++;
-		free(str);
-		free(str2);
-	}
+	//char *str2;
+	fd = open("textpru.txt", O_RDWR);
+	//fd2 = open("texto2", O_RDWR);
+	str = get_next_line(fd);
+	printf("\n&%s&\n", str);
+	str = get_next_line(fd);
+	printf("\n&%s&", str);
+	free(str);
+	//str2 = get_next_line(fd2);
+	//printf("&\n%s&\n", str2);
+	//str2 = get_next_line(fd2);
+	//printf("&\n%s&\n", str2);
 	close(fd);
-	close(fd2);
+	//close(fd2);
 }
+
+//TO DO: 
+// - Primero que te recorte la linea (line) hasta el salto de linea
+// - Segundo guardar el resto en una variable estática
